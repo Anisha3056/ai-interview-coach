43406f9b77f62b31e15b6e7ddeb02a6c21d8aba4
Anisha Tenali <anishatenali@gmail.com>
2026-06-23 12:37:16 +0530

    Add JWT authentication and user registration

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 
-from models.auth import RegisterRequest
+from models.auth import RegisterRequest, LoginRequest
 from models.user import User
 
 from services.auth_service import (
-    hash_password
+    ALGORITHM, hash_password, verify_password, create_access_token, verify_token
 )
 
 from database.db import SessionLocal
 from fastapi import HTTPException
+from fastapi import Header
 
 router = APIRouter()
 
@@ -49,3 +50,77 @@ def register_user(
         "message":
         "User registered successfully"
     }
+    
+@router.post("/login")
+def login_user(
+    request: LoginRequest
+):
+
+    db = SessionLocal()
+
+    user = db.query(User).filter(
+        User.email == request.email
+    ).first()
+
+    if not user:
+
+        raise HTTPException(
+    status_code=401,
+    detail="Invalid email or password"
+)
+
+    if not verify_password(
+        request.password,
+        user.password_hash
+    ):
+
+        raise HTTPException(
+    status_code=401,
+    detail="Invalid email or password"
+)
+    token = create_access_token(
+        {
+            "sub":
+            user.email
+        }
+    )
+
+    return {
+        "access_token":
+        token,
+
+        "token_type":
+        "bearer"
+    }
+    
+@router.get("/me")
+def get_current_user(
+    authorization: str = Header(None)
+):
+
+    if not authorization:
+
+        return {
+            "message":
+            "Token missing"
+        }
+
+    token = authorization.replace(
+        "Bearer ",
+        ""
+    )
+
+    email = verify_token(token)
+
+    if not email:
+
+        return {
+            "message":
+            "Invalid token"
+        }
+
+    return {
+        "email":
+        email
+    }
+    
--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -1,17 +1,61 @@
 from passlib.hash import pbkdf2_sha256
+from jose import jwt
+from datetime import datetime, timedelta
+from jose import JWTError
 
+
+SECRET_KEY = "super-secret-key"
+
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def hash_password(password):
 
     return pbkdf2_sha256.hash(password)
 
 
-def verify_password(
-    plain_password,
-    hashed_password
-):
+def verify_password(plain_password,hashed_password):
 
     return pbkdf2_sha256.verify(
         plain_password,
         hashed_password
     )
+    
+
+
+def create_access_token(data):
+
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update(
+        {"exp": expire}
+    )
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+    
+def verify_token(token):
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        email = payload.get("sub")
+
+        return email
+
+    except JWTError:
+
+        return None
